dcb13f02ef707b9a0e8f8a6a9e397a7ab68f349a
zaldi nugraha <zaldinugraha2020@gmail.com>
2024-11-15 07:09:52 +0700

    feat: add film component

--- src/component/FIlm.jsx
+++ src/component/FIlm.jsx
@@ -8,6 +8,7 @@ const FIlm = () => {
 
   const searchFilm = async () => {
     try {
+      setLoading(true)
       const response = await axios.get("https://www.omdbapi.com/", {
         params: {
           apikey: "54e4f3e6",
@@ -22,17 +23,44 @@ const FIlm = () => {
       }
     } catch (error) {
       setError("Gagal mengambil data", error);
+    }finally{
+      setLoading(false)
     }
   };
 
-  useEffect(() => {
-    searchFilm();
-  }, [query]);
-  console.log(film);
+  // useEffect(() => {
+  //   searchFilm();
+  // }, [query]);
+  // console.log(film);
 
   return (
     <div>
-      
+      <input 
+      type="text" 
+      value={query} 
+      onChange={(e)=> setQuery(e.target.value)} 
+      placeholder="Enter film title"
+      style={{padding : "8px",width:"240px" , marginRight:"8px"
+      }}
+      />
+
+      <button onClick={searchFilm} disabled={loading || !query}>
+        {loading ? "Loading..." : "seacrh"}
+      </button>
+      {error && <p style={{color:"red"}}>{error}</p>}
+      {
+        film.Title && (
+          <div style={{marginTop : "16px"}}>
+            <h2>{film.Title}</h2>
+            <p>Year : {film.Year} </p>
+            <p>Genre : {film.Genre}</p>
+            <p>Plot : {film.Plot}</p>
+            {film.Poster && <img src={film.Poster} alt={film.Title}
+            style={{width : "250px"}}/>}
+
+          </div>
+        )
+      }
     </div>
   );
   
